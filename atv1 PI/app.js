4c8819ecc1856a3810378a0698df68578141ecc5
PHMA2003 <mottapedro399@gmail.com>
2024-05-01 16:59:03 -0300

    preparando o projeto node

--- atv1 PI/app.js
+++ atv1 PI/app.js
@@ -1,39 +1,18 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  // Analise a URL para obter o parâmetro "tabuada"
-  const queryData = url.parse(req.url, true).query;
-  const number = parseInt(queryData.tabuada) || 1;
-
-  // Crie a página HTML com a tabuada
-  const html = createHTMLTable(number);
-
-  // Configure o cabeçalho da resposta
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-
-  // Envie a página HTML como resposta
-  res.end(html);
+// Rota para gerar a tabuada
+app.get('/', (req, res) => {
+    const { tabuada = 1, sequencia = 10 } = req.query;
+    let html = '<h1>Tabuada do ' + tabuada + '</h1>';
+    for (let i = 0; i <= sequencia; i++) {
+        html += `<p>${tabuada} x ${i} = ${tabuada * i}</p>`;
+    }
+    res.send(html);
 });
 
-server.listen(8080, () => {
-  console.log('Servidor rodando em http://localhost:8080/');
+// Inicia o servidor na porta 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
-
-function createHTMLTable(number) {
-  let tableHTML = '<html><body>';
-  tableHTML += `<h1>Tabuada do ${number}</h1>`;
-  tableHTML += '<table border="1">';
-
-  for (let i = 1; i <= 10; i++) {
-    tableHTML += '<tr>';
-    tableHTML += `<td>${number} x ${i}</td>`;
-    tableHTML += `<td>${number * i}</td>`;
-    tableHTML += '</tr>';
-  }
-
-  tableHTML += '</table>';
-  tableHTML += '</body></html>';
-  return tableHTML;
-}
